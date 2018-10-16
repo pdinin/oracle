76f5c58c52cd6a8c99222011535ac4ccaf95f25a
Pete Dinin <oracle@swrac1.sherwin.com>
2018-10-16 09:48:58 -0400

    Apply patches

--- database/12.2.0.2/scripts/startDB.sh
+++ database/12.2.0.2/scripts/startDB.sh
@@ -21,20 +21,59 @@ fi;
 # Start Listener
 lsnrctl start
 
-if [ ! -f $ORACLE_BASE/oradata/$ORACLE_SID/12.2.0.1.180717_IS_INSTALLED ]; then
-  # Start database
+# Start database
   sqlplus / as sysdba << EOF
     STARTUP;
     alter pluggable database all open;
     exit;
 EOF
-  $ORACLE_HOME/OPatch/datapatch -verbose
-  rm $ORACLE_BASE/oradata/$ORACLE_SID/*IS_INSTALLED
-  touch $ORACLE_BASE/oradata/$ORACLE_SID/12.2.0.1.180717_IS_INSTALLED
-else
-  # Start database
+
+run_datapatch="datapatch"
+
+patch_status=`sqlplus -s / as sysdba << EOF
+set heading off;
+set pagesize 0;
+select decode(sum(counter),2,'patched','datapatch')
+from (
+  select count(1)*1 as counter from (
+    select PATCH_ID, max(action) keep (dense_rank last order by action_time) as action
+    from dba_registry_sqlpatch group by patch_id)
+    where action='APPLY' and patch_id in (to_number($OJVM_PATCH), to_number($RU_PATCH))
+  union all
+  select count(1)*10 as counter from (
+    select PATCH_ID, max(action) keep (dense_rank last order by action_time) as action
+    from dba_registry_sqlpatch group by patch_id)
+    where action='APPLY' and patch_id not in (to_number($OJVM_PATCH), to_number($RU_PATCH))
+)
+;
+exit;
+EOF`
+
+# Store return code from SQL*Plus
+ret=$?
+
+# SQL Plus execution was successful and PDB is open
+if [ $ret -eq 0 ] && [ "$patch_status" = "$run_datapatch" ]; then
+  echo " "
+  echo "*****************"
+  echo "Running datapatch"
+  echo "*****************"
+  echo " "
+
   sqlplus / as sysdba << EOF
+  shutdown immediate;
+  startup upgrade;
+  alter pluggable database all open upgrade;
+  exit;
+EOF
+
+  $ORACLE_HOME/OPatch/datapatch -verbose
+
+  sqlplus / as sysdba << EOF
+    shutdown immediate;
     STARTUP;
+    alter pluggable database all open;
     exit;
 EOF
-fi;
+fi
+
